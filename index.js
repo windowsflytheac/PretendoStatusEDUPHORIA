f3d1f055d8a9529d2a06301d949090dd4bf2b1a1
Milk-Cool <milan-pl@mail.ru>
2024-07-10 12:53:59 +0300

    basic checks

---
+++ index.js
@@ -0,0 +1,35 @@
+/**
+ * Checks the website availability.
+ * 
+ * @returns {boolean} Whether the website is available.
+ */
+const checkWebsite = new Promise(async resolve => {
+    const f = await fetch("https://pretendo.network/").catch(() => resolve(false));
+    resolve(f.status >= 200 && f.status <= 399);
+});
+
+/**
+ * Checks the account service availability.
+ * 
+ * @returns {boolean} Whether the account service is available.
+ */
+const checkAccounts = new Promise(async resolve => {
+    // Checking the PN_Jemma's Mii
+    const f = await fetch("http://account.pretendo.cc/v1/api/miis?pids=1599718445", {
+        "headers": {
+            "X-Nintendo-Client-ID": "a2efa818a34fa16b8afbc8a74eba3eda",
+            "X-Nintendo-Client-Secret": "c91cdb5658bd4954ade78533a339cf9a"
+        }
+    }).catch(() => resolve(false));
+    resolve(f.status >= 200 && f.status <= 399);
+});
+
+/**
+ * Checks the connection test service availability.
+ * 
+ * @returns {boolean} Whether the account service is available.
+ */
+const checkConntest = new Promise(async resolve => {
+    const f = await fetch("http://conntest.pretendo.cc").catch(() => resolve(false));
+    resolve(f.status >= 200 && f.status <= 399);
+});
